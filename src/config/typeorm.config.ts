c76a1f6577f0f8e557196f7a0f7d2cb881a0d6de
fuerback <felipefuerback@hotmail.com>
2023-07-06 14:06:33 -0300

    Add typeorm in the app module

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config'
+
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    entities: [__dirname + '/../**/*.entity.ts'],
+    synchronize: true,
+};
