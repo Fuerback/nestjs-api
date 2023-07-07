b6a6c9ea46d8d8d368bf2a198fe3c5a175b97022
fuerback <felipefuerback@hotmail.com>
2023-07-07 17:31:42 -0300

    Update getTaskById filtering by userId

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -21,8 +21,8 @@ export class TasksService {
     return this.taskRepository.getTasks(filterDto, user);
   }
 
-  async getTaskById(id: number): Promise<Task> {
-    const found = await this.taskRepository.findOne({ where: { id } });
+  async getTaskById(id: number, user: User): Promise<Task> {
+    const found = await this.taskRepository.findOne({ where: { id, userId: user.id } });
 
     if (!found) {
       throw new NotFoundException(`Task with ID "${id}" not found`);
@@ -45,11 +45,11 @@ export class TasksService {
     }
   }
 
-  async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
-    const task = await this.getTaskById(id);
-    task.status = status;
-    await task.save();
+  // async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
+  //   const task = await this.getTaskById(id);
+  //   task.status = status;
+  //   await task.save();
 
-    return task;
-  }
+  //   return task;
+  // }
 }
