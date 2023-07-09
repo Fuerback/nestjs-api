75e10e90e436ef2d4ef400a7da180dcff9840927
fuerback <felipefuerback@hotmail.com>
2023-07-09 17:08:36 -0300

    Add final unit tests for task service

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -6,13 +6,19 @@ import { TaskStatus } from "./task-status.enum";
 import { User } from "../auth/user.entity";
 import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
 import { NotFoundException } from "@nestjs/common";
+import { CreateTaskDto } from "./dto/create-task.dto";
 
 describe('TaskService', () => {
     let taskService: TasksService;
+
+    const newTask = {id: 1, title: 'Test task', description: 'Test desc', status: TaskStatus.OPEN}
     
     const mockedRepo = {
         getTasks: jest.fn(() => Promise.resolve(Promise<Task[]>)),
-        findOne: jest.fn(() => Promise.resolve({id: 1, title: 'Test task', description: 'Test desc', status: TaskStatus.OPEN})),
+        findOne: jest.fn(() => Promise.resolve(newTask)),
+        createTask: jest.fn(() => Promise.resolve(newTask)),
+        delete: jest.fn(() => Promise.resolve({affected: 1})),
+        save: jest.fn(() => Promise.resolve(newTask)),
       };
 
     beforeEach(async () => {
@@ -29,26 +35,59 @@ describe('TaskService', () => {
         taskService = module.get<TasksService>(TasksService);
       });
 
-    it('getTasks should be called one time', async () => {
+    it('get tasks should be called one time', async () => {
         taskService.getTasks(new GetTasksFilterDto(), new User());
 
         expect(mockedRepo.getTasks).toBeCalledTimes(1);
     });
 
-    it('getTaskById return a valid task', async () => {
+    it('get task by id return a valid task', async () => {
         const user = new User();
 
         const task = taskService.getTaskById(1, user);
 
         expect(mockedRepo.findOne).toBeCalledTimes(1);
-        expect(task).resolves.toEqual({id: 1, title: 'Test task', description: 'Test desc', status: TaskStatus.OPEN});
+        expect(task).resolves.toEqual(newTask);
     });
 
-    it('getTaskById not found task error', async () => {
+    it('get task by id not found task', async () => {
       mockedRepo.findOne = jest.fn(() => Promise.resolve(null));
       const user = new User();
 
       expect(taskService.getTaskById(1, user)).rejects.toThrow(NotFoundException);
       expect(mockedRepo.findOne).toBeCalledTimes(1);
-  });
+    });
+
+    it('create task', async () => {
+
+      const task = taskService.createTask(new CreateTaskDto(), new User());
+
+      expect(mockedRepo.createTask).toBeCalledTimes(1);
+      expect(task).resolves.toEqual(newTask);
+    });
+
+    it('delete task', async () => {
+
+      const result = taskService.deleteTask(1, new User());
+
+      expect(mockedRepo.delete).toBeCalledTimes(1);
+      expect(result).resolves.toEqual(undefined);
+    });
+
+    it('delete task task not found', async () => {
+      mockedRepo.delete = jest.fn(() => Promise.resolve({affected: 0}));
+
+      expect(taskService.deleteTask(1, new User())).rejects.toThrow(NotFoundException);
+      expect(mockedRepo.delete).toBeCalledTimes(1);
+    });
+
+    it('update task', async () => {
+      mockedRepo.findOne = jest.fn(() => Promise.resolve(new Task()));
+      jest.spyOn(Task.prototype, 'save').mockImplementationOnce(() => Promise.resolve(new Task()))
+
+      const task = taskService.updateTaskStatus(1, TaskStatus.OPEN, new User());
+      
+      expect(mockedRepo.findOne).toBeCalledTimes(1);
+      expect((await task).status).toEqual(TaskStatus.OPEN);
+    });
 });
