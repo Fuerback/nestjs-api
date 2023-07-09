c06fa8fd51ed1f2e01134586bb1d09a908796d12
fuerback <felipefuerback@hotmail.com>
2023-07-09 16:16:07 -0300

    Add more unit tests for taskService

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -4,13 +4,15 @@ import { Task } from "./task.entity";
 import { TaskRepository } from "./task.repository";
 import { TaskStatus } from "./task-status.enum";
 import { User } from "../auth/user.entity";
+import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
+import { NotFoundException } from "@nestjs/common";
 
 describe('TaskService', () => {
     let taskService: TasksService;
     
     const mockedRepo = {
-        // mock the repo `getTasks`
-        getTasks: jest.fn((id) => Promise.resolve(Promise<Task[]>)),
+        getTasks: jest.fn(() => Promise.resolve(Promise<Task[]>)),
+        findOne: jest.fn(() => Promise.resolve({id: 1, title: 'Test task', description: 'Test desc', status: TaskStatus.OPEN})),
       };
 
     beforeEach(async () => {
@@ -27,12 +29,26 @@ describe('TaskService', () => {
         taskService = module.get<TasksService>(TasksService);
       });
 
-    it('getTask should be called one time', async () => {
-        const filterDto = {status: TaskStatus.OPEN, search: 'Some search query'};
-        const user = new User();
-
-        taskService.getTasks(filterDto, user);
+    it('getTasks should be called one time', async () => {
+        taskService.getTasks(new GetTasksFilterDto(), new User());
 
         expect(mockedRepo.getTasks).toBeCalledTimes(1);
     });
+
+    it('getTaskById return a valid task', async () => {
+        const user = new User();
+
+        const task = taskService.getTaskById(1, user);
+
+        expect(mockedRepo.findOne).toBeCalledTimes(1);
+        expect(task).resolves.toEqual({id: 1, title: 'Test task', description: 'Test desc', status: TaskStatus.OPEN});
+    });
+
+    it('getTaskById not found task error', async () => {
+      mockedRepo.findOne = jest.fn(() => Promise.resolve(null));
+      const user = new User();
+
+      expect(taskService.getTaskById(1, user)).rejects.toThrow(NotFoundException);
+      expect(mockedRepo.findOne).toBeCalledTimes(1);
+  });
 });
