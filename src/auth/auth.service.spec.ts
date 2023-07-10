5d0f09ab91682dd2440ffabc6ea4c2add585226f
fuerback <felipefuerback@hotmail.com>
2023-07-10 09:09:12 -0300

    Add auth service unit tests

---
+++ src/auth/auth.service.spec.ts
@@ -0,0 +1,49 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { AuthService } from "./auth.service";
+import { UserRepository } from "./user.repository";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+import { JwtService } from "@nestjs/jwt";
+
+describe('AuthService', () => {
+    let authService: AuthService;
+
+    const mockedRepo = {
+        signUp: jest.fn(() => Promise.resolve()),
+        validateUserPassword: jest.fn(() => Promise.resolve('username')),
+    };
+
+    const mockedJwtService = {
+        sign: jest.fn(() => 'token')
+    };
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+          providers: [
+            AuthService,
+            {
+                provide: UserRepository,
+                useValue: mockedRepo,
+            },
+            {
+                provide: JwtService,
+                useValue: mockedJwtService,
+            },
+        ],
+        }).compile();
+    
+        authService = module.get<AuthService>(AuthService);
+      });
+
+    it('sign up new user', async () => {
+        authService.signUp(new AuthCredentialsDto());
+
+        expect(mockedRepo.signUp).toBeCalledTimes(1);
+    });
+
+    it('sign in user', async () => {
+        const token = authService.signIn(new AuthCredentialsDto());
+
+        expect(mockedRepo.validateUserPassword).toBeCalledTimes(1);
+        expect((await token).accessToken).toEqual('token');
+    });
+});
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,27 +9,22 @@ import { JwtPayload } from './jwt-payload.interface';
 export class AuthService {
   constructor(
     private readonly userRepository: UserRepository,
-    private readonly dataSource: DataSource,
     private jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
-    return this.dataSource.transaction((manager) => {
-      return this.userRepository.signUp(authCredentialsDto);
-    });
+    return this.userRepository.signUp(authCredentialsDto);
   }
 
   async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{accessToken: string}> {
-    return this.dataSource.transaction(async (manager) => {
-      const username = await this.userRepository.validateUserPassword(authCredentialsDto);
-      if (!username) {
-        throw new UnauthorizedException('Invalid credentials');
-      }
+    const username = await this.userRepository.validateUserPassword(authCredentialsDto);
+    if (!username) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
 
-      const payload: JwtPayload = { username };
-      const accessToken = await this.jwtService.sign(payload);
+    const payload: JwtPayload = { username };
+    const accessToken = await this.jwtService.sign(payload);
 
-      return { accessToken };
-    });
+    return { accessToken };
   }
 }
